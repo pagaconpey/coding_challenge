55f4ef9f2d94a7b7e56ea3b951c1c50b819e823c
RazFernandez <mr.fernandez.mx@outlook.com>
2025-08-02 13:59:37 -0500

    ✨ feat: GridNotes component created
    This component arranges notes and it's useful for upcoming pagination component

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,16 +1,12 @@
 import TextInputIcon from "@/components/ui/textInputIcon";
-import NoteCard from "@/components/blocks/noteCard";
+import GridNotes from "@/components/layout/gridNotes";
 import { mockNotes } from "@/mock_data/mockNotes";
 
 export default function Home() {
   return (
     <>
       <TextInputIcon placeholder="Escribe una nueva nota..." />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {mockNotes.map((note, index) => (
-          <NoteCard key={index} {...note} />
-        ))}
-      </div>
+      <GridNotes notes={mockNotes}/>
     </>
   );
 }
--- website/src/components/layout/gridNotes.tsx
+++ website/src/components/layout/gridNotes.tsx
@@ -1,2 +1,16 @@
+import NoteCard from "../blocks/noteCard";
+import { NoteCardProps } from "../blocks/noteCard";
 
+type GridNotesProps = {
+    notes: NoteCardProps[];
+};
 
+export default function GridNotes({ notes }: GridNotesProps) {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {notes.map((note, index) => (
+                <NoteCard key={index} {...note} />
+            ))}
+        </div>
+    );
+}
