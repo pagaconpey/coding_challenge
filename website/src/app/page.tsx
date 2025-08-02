63abac95262fafe95b78d0d51d5ddb8104e7fc22
RazFernandez <mr.fernandez.mx@outlook.com>
2025-08-02 14:31:07 -0500

    ✨ feat: Added TextArea Component
    Reusable TextArea component ready to be implemented toc reate the take notes components

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,12 +1,14 @@
 import TextInputIcon from "@/components/ui/textInputIcon";
 import GridNotes from "@/components/layout/gridNotes";
 import { mockNotes } from "@/mock_data/mockNotes";
+import TextArea from "@/components/ui/textArea";
 
 export default function Home() {
   return (
     <>
       <TextInputIcon placeholder="Escribe una nueva nota..." />
-      <GridNotes notes={mockNotes}/>
+      <TextArea placeholder="Escribe una nueva nota..." rows={12} />
+      <GridNotes notes={mockNotes} />
     </>
   );
 }
