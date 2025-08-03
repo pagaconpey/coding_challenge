dff9497f54a4992f75ad876223572be4bfc9f4bb
RazFernandez <mr.fernandez.mx@outlook.com>
2025-08-02 22:39:48 -0500

    ♻️  refact: API KEY is ensured to be secretv
    App Sync key was replace and know is completely secret

--- website/src/lib/apolloClient.ts
+++ website/src/lib/apolloClient.ts
@@ -1,11 +1,16 @@
 // lib/apolloClient.ts
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
+const appSyncConfig = {
+    apiKey: `${process.env.NEXT_PUBLIC_DYNAMO_DB_API_KEY}`,
+    apiDomain: `${process.env.NEXT_PUBLIC_DYNAMO_DB_API_URL}`
+}
+
 const client = new ApolloClient({
     link: createHttpLink({
-        uri: "https://fdg37dqijzb5bik5ci7ll4w7la.appsync-api.us-east-2.amazonaws.com/graphql",
+        uri: appSyncConfig.apiDomain,
         headers: {
-            "x-api-key": "da2-xx22kxmcg5bcxhi74rauausswa",
+            "x-api-key": appSyncConfig.apiKey,
         },
     }),
     cache: new InMemoryCache(),
