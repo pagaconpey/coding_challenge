7db9b2d9ddaee4012daad2c959a7d8a7074a5428
RazFernandez <mr.fernandez.mx@outlook.com>
2025-08-03 18:04:16 -0500

    ✨feat: Added functionality to filters by sentiment

--- website/src/components/blocks/sentimentFilterSelector.tsx
+++ website/src/components/blocks/sentimentFilterSelector.tsx
@@ -3,11 +3,13 @@
 import SelectSentiment from "../ui/selectSentiment";
 import { Sentiment } from "@/types/sentiment";
 import { SENTIMENT_OPTIONS_WITH_ALL } from "@/types/select-options";
-import { useState } from "react";
 
-export default function SentimentFilterSelector() {
-    const [sentiment, setSentiment] = useState<Sentiment>("All");
+type Props = {
+    sentiment: Sentiment;
+    setSentiment: (sentiment: Sentiment) => void;
+};
 
+export default function SentimentFilterSelector({ sentiment, setSentiment }: Props) {
     return (
         <div className="flex flex-col lg:flex-row lg:items-center gap-2 lg:gap-4">
             <p className="text-sm font-medium">Filtrar por sentimiento:</p>
--- website/src/components/blocks/yourNotesSection.tsx
+++ website/src/components/blocks/yourNotesSection.tsx
@@ -1,10 +1,25 @@
+"use client"
+
 import HorizontalCenter from "../layout/horizontalCenter";
 import HorizontalEnd from "../layout/horizontalEndProps";
 import GridNotes from "../layout/gridNotes";
 import SentimentFilterSelector from "./sentimentFilterSelector";
+import { useState, useMemo } from "react";
+import { Sentiment } from "@/types/sentiment";
 import { NoteCardProps } from "./noteCard";
 
 export default function YourNotesSection({ sortedNotes }: { sortedNotes: NoteCardProps[] }) {
+
+    const [sentiment, setSentiment] = useState<Sentiment>("All");
+
+    const filteredNotes = useMemo(() => {
+        if (sentiment === "Happy") return sortedNotes.filter(note => note.sentiment === "happy")
+        if (sentiment === "Sad") return sortedNotes.filter(note => note.sentiment === "sad")
+        if (sentiment === "Angry") return sortedNotes.filter(note => note.sentiment === "angry")
+        if (sentiment === "Neutral") return sortedNotes.filter(note => note.sentiment === "neutral")
+        return sortedNotes;
+    }, [sentiment, sortedNotes]);
+
     return (
         <div className="max-w-7xl mx-auto px-4 pb-8">
             <HorizontalCenter className="pt-8 pb-8">
@@ -12,10 +27,10 @@ export default function YourNotesSection({ sortedNotes }: { sortedNotes: NoteCar
             </HorizontalCenter>
 
             <HorizontalEnd className="mb-8 mr-4">
-                <SentimentFilterSelector />
+                <SentimentFilterSelector sentiment={sentiment} setSentiment={setSentiment} />
             </HorizontalEnd>
 
-            <GridNotes notes={sortedNotes} />
+            <GridNotes notes={filteredNotes} />
         </div>
     );
 }
